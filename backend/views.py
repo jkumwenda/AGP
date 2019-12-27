24b4ca4ab35dcc3414a9537d9add1d9d33c7c600
jkumwenda <jkumwenda@gmail.com>
2019-12-27 14:51:51 +0200

    Accepted view changes changes

--- backend/views.py
+++ backend/views.py
@@ -3,13 +3,10 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 from .serializers import *
 from .models import *
-<<<<<<< HEAD
 from .views_helper import *
 
-=======
 from rest_framework.response import Response
 from rest_framework.decorators import action
->>>>>>> louis
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
